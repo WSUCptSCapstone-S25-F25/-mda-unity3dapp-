3f141f55793a70709545ee3d2e9dfadbe3302e0b
cristobal-escobar <cristobal.escobar@wsu.edu>
2025-09-23 12:19:46 -0700

    Added tables Shifts and Volunteer hours to schema file

--- Fiz Project/DB Files/schema.sql
+++ Fiz Project/DB Files/schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE Items (
     Category VARCHAR(100) NOT NULL,
     Quantity INT NOT NULL DEFAULT 0,
     Exp_date DATE,
-    % Barcode = UNIQUE
+    -- Barcode = UNIQUE
     
 );
 
@@ -20,7 +20,7 @@ CREATE TABLE Students (
 CREATE TABLE Volunteers (
     VolunteerID INT AUTOINCREMENT PRIMARY KEY,
     Name VARCHAR(100) NOT NULL,
-    -- Role ? 
+    -- Role = Admin, user, student ? 
     -- Password ?
 );
 
@@ -29,9 +29,31 @@ CREATE TABLE Volunteers (
     UsageID INT AUTOINCREMENT PRIMARY KEY,
     StudentId INT NOT NULL,
     Visit_date DATE DEFAULT (CURRENT_DATE), 
+    Total_items INT DEFAULT 0,
 
-    FOREIGN KEY (StudentId) REFERENCES Students(StudentId)
-     ON DELETE CASCADE
+    FOREIGN KEY (StudentId) REFERENCES Students(StudentId) ON DELETE CASCADE,
+    FOREIGN KEY (ItemId) REFERENCES Items(ItemId) ON DELETE CASCADE
 
- )
+ );
+
+
+CREATE TABLE Shifts (
+    ShiftId INT AUTOINCREMENT PRIMARY KEY,
+    Shift_date DATE NOT NULL,
+    Shift_start DATE NOT NULL,
+    Shift_end DATE NOT NULL;
+)
+
+
+CREATE TABLE VolunteerHours (
+    HoursId INT AUTOINCREMENT PRIMARY KEY,
+    VolunteerID INT NOT NULL,
+    ShiftId INT NOT NULL,
+    -- Worked_hours INT?
+
+    FOREIGN KEY (VolunteerID) REFERENCES Volunteers(VolunteerID) ON DELETE CASCADE,
+    FOREIGN KEY (ShiftId) REFERENCES Shifts(ShiftId) ON DELETE CASCADE
+)
+
+
 

5eaa2258ed4d158a9e04971fc5a6c1c4eed4c0cc
cristobal-escobar <cristobal.escobar@wsu.edu>
2025-10-09 15:13:51 -0700

    Updated app for previous changes in schema file, added edit item option

--- Fiz Project/App/DB Files/seed_data.sql
+++ Fiz Project/App/DB Files/seed_data.sql
@@ -7,20 +7,20 @@ INSERT INTO Items (Name, Category, Quantity, ExpDate) VALUES
     ('item4', 'category1', 5, '2025-09-22'),
     ('item5', 'category2', 6, '2024-09-22');
 
-INSERT INTO Volunteers (Name) VALUES
-    ('name1'),
-    ('name3'),
-    ('name4'),
-    ('name2');
+-- INSERT INTO Volunteers (Name) VALUES
+--     ('name1'),
+--     ('name3'),
+--     ('name4'),
+--     ('name2');
 
 
-INSERT INTO PantryUsage (StudentId, VisitDate) VALUES
-    (01020304, '2024-09-22'),
-    (01020302, '2024-09-22'),
-    (01020303, '2024-09-22'),
-    (01020301, '2024-09-22'),
-    (01020305, '2024-09-22'),
-    (01020306, '2024-09-22');
+-- INSERT INTO PantryUsage (StudentId, VisitDate) VALUES
+--     (01020304, '2024-09-22'),
+--     (01020302, '2024-09-22'),
+--     (01020303, '2024-09-22'),
+--     (01020301, '2024-09-22'),
+--     (01020305, '2024-09-22'),
+--     (01020306, '2024-09-22');
 
 
 
--- Fiz Project/App/app.py
+++ Fiz Project/App/app.py
@@ -54,7 +54,7 @@ def add_item():
 
         if db_connection:
             cursor = db_connection.cursor()
-            cursor.execute ("INSERT INTO Items (Name, Category, Quantity, Exp_date, Barcode)" \
+            cursor.execute ("INSERT INTO Items (Name, Category, Quantity, ExpDate, Barcode)" \
             " VALUES (%s, %s, %s, %s, %s)", (name, category, quantity, exp_date, barcode))
 
             db_connection.commit()
@@ -64,6 +64,37 @@ def add_item():
         return redirect(url_for('inventory'))
 
     return render_template('add_item.html')
+
+# route to edit item
+@app.route('/edit_item/<int:item_id>', methods = ['GET', 'POST'])
+def edit_item(item_id):
+    db_connection = get_db_connection()
+    cursor = db_connection.cursor(dictionary=True)
+
+    if request.method == 'POST':
+        name = request.form['name']
+        category = request.form['category']
+        quantity = request.form['quantity']
+        exp_date = request.form['exp_date']
+
+        cursor.execute ("UPDATE Items SET Name=%s, Category=%s, Quantity=%s, ExpDate=%s WHERE ItemId=%s",
+                        (name, category, quantity, exp_date, item_id))
+        
+
+        db_connection.commit()
+        cursor.close()
+        db_connection.close()
+        return redirect(url_for('inventory'))
+    
+    cursor.execute("SELECT * FROM Items WHERE ItemId=%s", (item_id,))
+    item = cursor.fetchone()
+    cursor.close()
+    db_connection.close()
+
+    if item and item['ExpDate']:
+        item["ExpDate"] = item["ExpDate"].strftime('%Y-%m-%d')
+
+    return render_template('edit_item.html', item=item)
 
 
 
